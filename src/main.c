70fde55d20c069e1cde0b12d9dc95b2d1c425d61
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-03-01 09:05:04 +1100

    Added raylib basic web example to src/main.c

---
+++ src/main.c
@@ -0,0 +1,40 @@
+// Basic example (raylib)
+// https://github.com/raysan5/raylib
+#include "raylib.h"
+
+// #define PLATFORM_WEB
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+void UpdateDrawFrame(void);
+
+int main(void)
+{
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        UpdateDrawFrame();
+    }
+#endif
+
+    CloseWindow();
+}
+
+void UpdateDrawFrame(void)
+{
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+    EndDrawing();
+}
